37e6a2c5d4c420b9e1e09114cebc00308710866b
novice1031 <sanaraza382@gmail.com>
2023-06-20 02:20:47 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+
+import os
+import sys
+import string
+
+
+def create_index(filenames, index, file_titles):
+    """
+    This function is passed:
+        filenames:      a list of file names (strings)
+
+        index:          a dictionary mapping from terms to file names (i.e., inverted index)
+                        (term -> list of file names that contain that term)
+
+        file_titles:    a dictionary mapping from a file names to the title of the article
+                        in a given file
+                        (file name -> title of article in that file)
+
+    The function will update the index passed in to include the terms in the files
+    in the list filenames.  Also, the file_titles dictionary will be updated to
+    include files in the list of filenames."""
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            title = f.readline().strip()
+            file_titles[filename] = title
+
+            content = f.read()
+            terms = content.split()
+
+            for term in terms:
+                normalized_term = term.lower().strip(string.punctuation)
+
+                if normalized_term != '' and set(normalized_term) != set(string.punctuation):
+                    if normalized_term in index:
+                        index[normalized_term].append(filename)
+                    else:
+                        index[normalized_term] = [filename]
+
+def search(index, query):
+    """
+    This function is passed:
+        index:      a dictionary mapping from terms to file names (inverted index)
+                    (term -> list of file names that contain that term)
+
+        query  :    a query (string), where any letters will be lowercase
+
+    The function returns a list of the names of all the files that contain *all* of the
+    terms in the query (using the index passed in)."""
+
+
+    terms = query.split()
+
+    # Initialize an empty result list
+    result = []
+
+    # Find the first term in the index
+    for term in terms:
+        if term in index:
+            result = list(set(index[term]))
+            break
+
+    # Iterate over the remaining terms and update the result list accordingly
+    for term in terms:
+        if term in index:
+            result = list(set(result) & set(index[term]))
+        else:
+            # If term is not in index, the intersection will be empty, so we can break early
+            result = []
+            break
+
+    return result
+
+##### YOU SHOULD NOT NEED TO MODIFY ANY CODE BELOW THIS LINE (UNLESS YOU'RE ADDING EXTENSIONS) #####
+
+
+def do_searches(index, file_titles):
+    """
+    This function is given an inverted index and a dictionary mapping from
+    file names to the titles of articles in those files.  It allows the user
+    to run searches against the data in that index.
+    """
+    while True:
+        query = input("Query (empty query to stop): ")
+        query = query.lower()                   # convert query to lowercase
+        if query == '':
+            break
+        results = search(index, query)
+
+        # display query results
+        print("Results for query '" + query + "':")
+        if results:                             # check for non-empty results list
+            for i in range(len(results)):
+                title = file_titles[results[i]]
+                print(str(i + 1) + ".  Title: " + title + ",  File: " + results[i])
+        else:
+            print("No results match that query.")
+
+
+def textfiles_in_dir(directory):
+    """
+    Given the name of a valid directory, returns a list of the .txt
+    file names within it.
+
+    Input:
+        directory (string): name of directory
+    Returns:
+        list of (string) names of .txt files in directory
+    """
+    filenames = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            filenames.append(os.path.join(directory, filename))
+
+    return filenames
+
+
+def main():
+    """
+    Usage: searchengine.py <file directory> -s
+    The first argument specified should be the directory of text files that
+    will be indexed/searched.  If the parameter -s is provided, then the
+    user can interactively search (using the index).  Otherwise (if -s is
+    not included), the index and the dictionary mapping file names to article
+    titles are just printed on the console.
+    """
+    # Get command line arguments
+    args = sys.argv[1:]
+
+    num_args = len(args)
+    if num_args < 1 or num_args > 2:
+        print('Please specify directory of files to index as first argument.')
+        print('Add -s to also search (otherwise, index and file titles will just be printed).')
+    else:
+        # args[0] should be the folder containing all the files to index/search.
+        directory = args[0]
+        if os.path.exists(directory):
+            # Build index from files in the given directory
+            files = textfiles_in_dir(directory)
+            index = {}          # index is empty to start
+            file_titles = {}    # mapping of file names to article titles is empty to start
+            create_index(files, index, file_titles)
+
+            # Either allow the user to search using the index, or just print the index
+            if num_args == 2 and args[1] == '-s':
+                do_searches(index, file_titles)
+            else:
+                print('Index:')
+                print(index)
+                print('File names -> document titles:')
+                print(file_titles)
+        else:
+            print('Directory "' + directory + '" does not exist.')
+
+
+if __name__ == '__main__':
+    main()
